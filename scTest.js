3a4071cf634d2f67495ec48fd68b0761fbb40421
Steven Cameron <stcamero@ucsc.edu>
2018-05-07 23:21:11 -0700

    Branched and added more

--- scTest.js
+++ scTest.js
@@ -1,5 +1,9 @@
 // This is just a test
 var a = 2;
 var b = 3;
-var c = true;
-var d = "Hello there Steven";
+var c = false;
+var d = "Hello there Steven. This is test 2";
+
+// this is a comment
+
+var e = 7;
